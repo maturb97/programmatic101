b3c0747d66321c157501a3f6c93ec22bb054e81e
Mateusz <maturb97@gmail.com>
2025-06-06 20:33:27 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/programmatic101/',
+  base: '/programmatic101/', // MUSI być dokładnie nazwa repo
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
